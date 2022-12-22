722db948bc9c8fa6b710a152547ebc8b8615d3cb
Zackory Kelly <zackkelly@live.ca>
2022-12-22 02:57:17 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <div className="bg-gray-700 min-h-screen flex justify-center items-center">
+    <div className="hero bg-gray-700 min-h-screen flex justify-center items-center">
       <div className="text-white text-[26px]">Coming Soon!</div>
       <Routes>
         <Route path="/" element={<h1 className="text-2xl text-center"> </h1>} />
