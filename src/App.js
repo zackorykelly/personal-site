674b923778685630bd4bd6584b5d24bd7108747f
Zackory Kelly <zackkelly@live.ca>
2022-12-29 00:37:20 -0500

    add header and footer, home image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
+
+  //Temp - returns centered coming soon message for incomplete pages
+  const comingSoon = () => {
+    return (
+      <div className="mx-auto my-auto">
+        <div className="text-white text-[26px]">Coming Soon!</div>
+      </div>
+    )
+  }
+
   return (
     <>
-    <div className="hero bg-gray-700 min-h-screen flex justify-center items-center">
-      <div className="text-white text-[26px]">Coming Soon!</div>
-      <Routes>
-        <Route path="/" element={<h1 className="text-2xl text-center"> </h1>} />
-        <Route path="/about" element={<h1 className="text-2xl text-center"> </h1>} />
-        <Route path="/portfolio" element={<h1 className="text-2xl text-center"> </h1>} />
-        <Route path="/experience" element={<h1 className="text-2xl text-center"> </h1>} />
-        <Route path="/contact" element={<h1 className="text-2xl text-center"> </h1>} />
-      </Routes>
+    <div className="min-h-screen flex flex-col">
+      <Navbar/>
+      <div className="hero grow flex flex-col">
+        <div className="max-w-5xl mx-auto p-4 grow flex flex-col">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={comingSoon()} />
+            <Route path="/portfolio" element={comingSoon()} />
+            <Route path="/experience" element={comingSoon()} />
+            <Route path="/contact" element={comingSoon()} />
+          </Routes>
+        </div>
+      </div>
+      <Footer/>
     </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         primary: '#0074d9',
+         secondary: '#77d4fc',
+      }
+    },
   },
   plugins: [],
 }
